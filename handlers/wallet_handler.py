048ad938504dddba2f3831d94acfb608814cb9ac
cal-cmd <calebster.j@gmail.com>
2024-06-29 22:07:14 +0000

    updated wallet generation (error with key), updated gitignore

--- handlers/wallet_handler.py
+++ handlers/wallet_handler.py
@@ -6,5 +6,5 @@ from scripts.generate_wallet import create_wallet
 async def generate_wallet(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     wallet = create_wallet()
     await update.message.reply_text(
-        f'Your new wallet address is: {wallet["address"]}\nYour private key is: {wallet["private_key"]}'
+        f'Your new wallet address is:\n{wallet["address"]}\n\nYour private key is:\n{wallet["private_key"]}'
     )
--- scripts/generate_wallet.py
+++ scripts/generate_wallet.py
@@ -4,4 +4,4 @@ from web3 import Web3
 def create_wallet():
     web3 = Web3()
     account = web3.eth.account.create()
-    return {'address': account.address, 'private_key': account.privateKey.hex()}
+    return {'address': account.address, 'private_key': account.key.hex()}
